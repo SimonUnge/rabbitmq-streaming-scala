e7727e2d0b3f069f14b9be033ae7078ae4068f20
Simon Unge <simunge@amazon.com>
2025-12-02 22:56:19 -0800

    more codec

--- src/main/scala/rabbitmq/streaming/Models.scala
+++ src/main/scala/rabbitmq/streaming/Models.scala
@@ -1,14 +1,14 @@
 package rabbitmq.streaming
 
 case class DeclarePublisherRequest(
-  publisherId: Byte,
-  stream: String,
-  publisherReference: Option[String] = None
+    publisherId: Byte,
+    stream: String,
+    publisherReference: Option[String] = None
 )
 
 case class DeclarePublisherResponse(
-  correlationId: Int,
-  responseCode: Short
+    correlationId: Int,
+    responseCode: Short
 )
 
 sealed trait DeclarePublisherResult
@@ -16,21 +16,36 @@ object DeclarePublisherResult {
   case object Success extends DeclarePublisherResult
   case object StreamDoesNotExist extends DeclarePublisherResult
   case object AccessRefused extends DeclarePublisherResult
-  case class UnknownError(code: Short) extends DeclarePublisherResult  // catch-all
+  case class UnknownError(code: Short)
+      extends DeclarePublisherResult // catch-all
 }
 
 case class PeerPropertiesRequest(
-  properties: Map[String, String]
+    properties: Map[String, String]
 )
 
 case class PeerPropertiesResponse(
-  correlationId: Int,
-  responseCode: Short,
-  properties: Map[String, String]
+    correlationId: Int,
+    responseCode: Short,
+    properties: Map[String, String]
 )
 
 sealed trait PeerPropertiesResult
 object PeerPropertiesResult {
-    case class Success(serverProperties: Map[String, String]) extends PeerPropertiesResult
-    case class UnknownError(code: Short) extends PeerPropertiesResult  // catch-all
+  case class Success(serverProperties: Map[String, String])
+      extends PeerPropertiesResult
+  case class UnknownError(code: Short) extends PeerPropertiesResult // catch-all
+}
+
+case class SaslHandshakeRequest()
+case class SaslHandshakeResponse(
+    correlationId: Int,
+    responseCode: Short,
+    mechanisms: List[String]
+)
+
+sealed trait SaslHandshakeResult
+object SaslHandshakeResult {
+  case class Success(mechanisms: List[String]) extends SaslHandshakeResult
+  case class UnknownError(code: Short) extends SaslHandshakeResult
 }
---
+++ src/main/scala/rabbitmq/streaming/SaslHandshakeCodec.scala
@@ -0,0 +1,42 @@
+package rabbitmq.streaming
+
+import java.nio.ByteBuffer
+
+object SaslHandshakeCodec {
+
+  def encode(correlationId: Int): ByteBuffer = {
+    // Fixed size: 2 (key) + 2 (version) + 4 (correlationId)
+    val totalSize = 2 +  // Key   
+                    2 +  // Version   
+                    4    // CorrelationId
+
+    val buffer = Protocol.allocate(totalSize)
+
+    buffer.putShort(Protocol.Commands.SaslHandshake)
+    buffer.putShort(Protocol.ProtocolVersion)
+    buffer.putInt(correlationId)
+
+    buffer
+  }
+  
+  def decode(buffer: ByteBuffer): Either[String, SaslHandshakeResponse] = {
+    for {
+        key <- Right(buffer.getShort()).filterOrElse(
+        _ == Protocol.Commands.SaslHandshakeResponse,
+        s"Invalid key field"
+        )
+        version <- Right(buffer.getShort()).filterOrElse(
+        _ == Protocol.ProtocolVersion,
+        s"Incompatible protocol version"
+        )
+        correlationId = buffer.getInt()
+        responseCode  = buffer.getShort()
+        numMechanisms = buffer.getInt()
+        mechanisms <- Right {
+          (0 until numMechanisms).map { _ =>
+            Protocol.readString(buffer)
+          }.toList
+        }
+    } yield SaslHandshakeResponse(correlationId, responseCode, mechanisms)
+  }
+}
