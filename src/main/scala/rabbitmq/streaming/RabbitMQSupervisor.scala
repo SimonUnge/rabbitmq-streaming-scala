01ac23713a779c1f7d420debcf566ca04ef4533a
Simon Unge <simunge@amazon.com>
2025-12-21 23:47:32 -0800

    Add API to create connection, subscriber and publisher via the RMQ supervisor

--- src/main/scala/rabbitmq/streaming/RabbitMQSupervisor.scala
+++ src/main/scala/rabbitmq/streaming/RabbitMQSupervisor.scala
@@ -4,14 +4,30 @@ import org.apache.pekko.actor.typed.{ActorRef, Behavior}
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
 
 object RabbitMQSupervisor {
-  
   sealed trait Command
+  case class CreateConnection(
+      config: ConnectionConfig,
+      replyTo: ActorRef[ActorRef[ConnectionActor.Command]]
+  ) extends Command
+  case class CreatePublisher(
+      connectionActor: ActorRef[ConnectionActor.Command],
+      stream: String,
+      replyTo: ActorRef[ActorRef[PublisherActor.Command]]
+  ) extends Command
+  case class CreateSubscriber(
+      connectionActor: ActorRef[ConnectionActor.Command],
+      stream: String,
+      offsetSpec: OffsetSpecification,
+      initialCredit: Short,
+      messageHandler: OsirisChunk => Unit,
+      replyTo: ActorRef[ActorRef[SubscriberActor.Command]]
+  ) extends Command
   case object Shutdown extends Command
-  
+
   def apply(): Behavior[Command] =
     Behaviors.setup { context =>
       context.log.info("RabbitMQSupervisor starting")
-      
+
       // Spawn the three child supervisors
       val connectionSupervisor = context.spawn(
         ConnectionSupervisor(),
@@ -25,30 +41,59 @@ object RabbitMQSupervisor {
         SubscriberSupervisor(),
         "subscriber-supervisor"
       )
-      
+
       context.log.info("All supervisors started")
-      
+
       running(connectionSupervisor, publisherSupervisor, subscriberSupervisor)
     }
-  
+
   private def running(
-    connectionSupervisor: ActorRef[ConnectionSupervisor.Command],
-    publisherSupervisor: ActorRef[PublisherSupervisor.Command],
-    subscriberSupervisor: ActorRef[SubscriberSupervisor.Command]
+      connectionSupervisor: ActorRef[ConnectionSupervisor.Command],
+      publisherSupervisor: ActorRef[PublisherSupervisor.Command],
+      subscriberSupervisor: ActorRef[SubscriberSupervisor.Command]
   ): Behavior[Command] =
-    Behaviors.receive { (context, message) =>
-      message match {
-        case Shutdown =>
-          context.log.info("Shutting down RabbitMQ system")
-          Behaviors.stopped
-        
-        case _ =>
-          Behaviors.unhandled
+    Behaviors
+      .receive[Command] { (context, message) =>
+        message match {
+          case CreateConnection(config, replyTo) =>
+            connectionSupervisor ! ConnectionSupervisor.CreateConnection(
+              config,
+              replyTo
+            )
+            Behaviors.same
+          case CreatePublisher(connection, stream, replyTo) =>
+            publisherSupervisor ! PublisherSupervisor.CreatePublisher(
+              connection,
+              stream,
+              replyTo
+            )
+            Behaviors.same
+          case CreateSubscriber(
+                connection,
+                stream,
+                offset,
+                credit,
+                handler,
+                replyTo
+              ) =>
+            subscriberSupervisor ! SubscriberSupervisor.CreateSubscriber(
+              connection,
+              stream,
+              offset,
+              credit,
+              handler,
+              replyTo
+            )
+            Behaviors.same
+          case Shutdown =>
+            context.log.info("Shutting down RabbitMQ system")
+            Behaviors.stopped
+          case _ =>
+            Behaviors.unhandled
+        }
       }
-    }
-    .receiveSignal {
-      case (context, org.apache.pekko.actor.typed.PostStop) =>
+      .receiveSignal { case (context, org.apache.pekko.actor.typed.PostStop) =>
         context.log.info("RabbitMQSupervisor stopped")
         Behaviors.same
-    }
+      }
 }
